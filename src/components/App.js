eeb67d04129f30e3a5bab2d3a35dd8b15801eee4
sahil n <sahil@mutterfly.in>
2018-12-27 17:59:58 +0530

    includes react-router example

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            Page One
+            <Link to="/two"> Two </Link>
+        </div>
+            )
+}
+
+const PageTwo = () => {
+    return (
+        <div>
+            Page Two
+            <Link to="/">One</Link>
+        </div>
+        )
+}
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/two" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App;
