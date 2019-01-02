b6be4f505f46c21d05171c907e03fdaae2480fa6
sahil n <sahil@mutterfly.in>
2019-01-02 13:37:14 +0530

    adds redux-form and renders fields in the create new stream component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
+class StreamCreate extends Component {
+
+    renderInput({input, label}) {             //uses destructring to get ths input key out of the formProps object.
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input}/>   
+            </div>
+            
+        )
+    }
+
+    render() {
+        return (
+            <form className="ui form" >
+                <Field name="title" component={this.renderInput} label="Give your Stream a title"/>
+                <Field name="description" component={this.renderInput} label="Describe your Stream!"/>
+            </form>
+        )
+    }
 }
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate'            // usually named after the purpose of the form
+})(StreamCreate);
