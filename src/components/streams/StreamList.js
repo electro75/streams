b3235dcb621fff7a87763fdf434164dd13d66486
sahil n <sahil@mutterfly.in>
2019-01-14 17:41:45 +0530

    adds user id to the streams object and conditionally renders admin buttons

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,10 +7,24 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream) {
+        if(stream.userId === this.props.currentUserId) {
+            return (
+            <div className="right floated content">
+                <button className="ui button primary"> Edit </button>
+                <button className="ui button negative"> Delete </button>
+            </div>
+            )
+        } else {
+            return null
+        }
+    }
+
     renderList() {
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera"></i>
                     <div className="content">
                         {stream.title}
@@ -34,7 +48,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    return {streams: Object.values(state.streams)}          // Object.values is an inbuilt JS function that turns all the values into an array
+    return {
+        streams : Object.values(state.streams),      // Object.values is an inbuilt JS function that turns all the values into an array
+        currentUserId: state.auth.userId
+    }
 }
 
 export default connect(mapStateToProps, {fetchStreams})(StreamList);
